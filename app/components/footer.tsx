4cd039419d5861e396ca73e0b9ec200bab520c79
yasser-chihab <yasser.chihab@journify.io>
2026-01-13 22:32:10 +0100

    feat: add LinkedIn and X links to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -30,6 +30,28 @@ export default function Footer() {
             <p className="ml-2 h-7">github</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/yasser-chihab"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">linkedin</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://x.com/YasserChihab7"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">x</p>
+          </a>
+        </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} MIT Licensed
