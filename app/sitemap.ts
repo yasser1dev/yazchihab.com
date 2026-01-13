f28b70d7cf2aed608705686bd6478ffc1ad34122
yasser-chihab <yasser.chihab@journify.io>
2026-01-13 21:56:57 +0100

    move blog section to writing section

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,14 @@
-import { getBlogPosts } from 'app/blog/utils'
+import { getWritingPosts } from 'app/writing/utils'
 
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
+  let blogs = getWritingPosts().map((post) => ({
+    url: `${baseUrl}/writing/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  let routes = ['', '/writing'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
--- app/writing/utils.ts
+++ app/writing/utils.ts
@@ -49,8 +49,8 @@ function getMDXData(dir) {
   })
 }
 
-export function getBlogPosts() {
-  return getMDXData(path.join(process.cwd(), 'app', 'blog', 'posts'))
+export function getWritingPosts() {
+  return getMDXData(path.join(process.cwd(), 'app', 'writing', 'posts'))
 }
 
 export function formatDate(date: string, includeRelative = false) {
